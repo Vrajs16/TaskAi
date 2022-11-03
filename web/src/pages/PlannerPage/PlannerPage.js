7d9319b72e66d62d26005cffd57422cec9a316f7
alonlerner <lernera97@gmail.com>
2022-11-03 12:32:59 -0400

    created a task view component and added a date picker and tasks and appointments boxes

--- web/src/pages/PlannerPage/PlannerPage.js
+++ web/src/pages/PlannerPage/PlannerPage.js
@@ -1,6 +1,7 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 import { Container, Tabs, TabList, TabPanel, TabPanels, Tab, HStack, Text } from '@chakra-ui/react'
+import TaskView from 'src/components/TaskView/TaskView'
 
 const PlannerPage = () => {
   return (
@@ -17,7 +18,7 @@ const PlannerPage = () => {
             </HStack>
             <TabPanels>
               <TabPanel>
-                <p>Tasks View!</p>
+                <TaskView></TaskView>
               </TabPanel>
               <TabPanel>
                 <p>Calender View!</p>
