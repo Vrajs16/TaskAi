2cf774892ae0820f2f941d3a923ed7ae58d052f4
ss3288 <87153998+ss3288@users.noreply.github.com>
2022-11-09 18:55:24 -0500

    Soham shah just created basic files for security team

--- documents/SohamShah.md
+++ documents/SohamShah.md
@@ -1,3 +1,6 @@
 #Soham Shah
 #Task 1 complete: Create a readme file
 #Task 2 ongoing: Finish the project
+#Task 3 On the security team and doing research
+#Task 4 Decided on Supabase auth, working out the kinks
+#Task 5 Working with Willis doing security things
--- web/src/App.js
+++ web/src/App.js
@@ -1,3 +1,5 @@
+import { AuthProvider } from '@redwoodjs/auth'
+import { createClient } from '@supabase/supabase-js'
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
@@ -11,15 +13,22 @@ import './index.css'
 
 const extendedTheme = extendTheme(theme)
 
+const supabaseClient = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
+  )
+
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <ColorModeScript />
-      <ChakraProvider theme={extendedTheme}>
-        <RedwoodApolloProvider>
-          <Routes />
-        </RedwoodApolloProvider>
-      </ChakraProvider>
+      <AuthProvider client={supabaseClient} type="supabase">
+        <ColorModeScript />
+        <ChakraProvider theme={extendedTheme}>
+          <RedwoodApolloProvider>
+            <Routes />
+          </RedwoodApolloProvider>
+        </ChakraProvider>
+      </AuthProvider>
     </RedwoodProvider>
   </FatalErrorBoundary>
 )
