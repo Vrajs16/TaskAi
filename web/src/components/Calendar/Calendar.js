1356dfcfe34ccf4671cb41df1a28334bd3218d1a
Villaire Pierre <vap49@njit.edu>
2022-11-15 01:08:19 -0500

    button attempt 1

--- web/src/components/Calendar/Calendar.js
+++ web/src/components/Calendar/Calendar.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import {useNavigate} from 'react-router-dom'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 
+import { Link, navigate, routes } from '@redwoodjs/router'
+import NewTaskPage from 'src/pages/Task/NewTaskPage/NewTaskPage'
+
 export default function (){
+
   return (
     <section>
+      {/* add button here to add route to new task page 
+        <button onClick={navigate(NewTaskPage)}>New Task</button> 
+      */}
+      <button>New Task</button>
       <FullCalendar
         plugins = {[dayGridPlugin]}
         initialView = "dayGridMonth"
--- web/src/pages/CalendarPage/CalendarPage.js
+++ web/src/pages/CalendarPage/CalendarPage.js
@@ -1,7 +1,8 @@
-import { Link, routes } from '@redwoodjs/router'
+import { Link, navigate, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 import Calendar from 'src/components/Calendar/Calendar.js'
+import NewTaskPage from '../Task/NewTaskPage/NewTaskPage'
 
 const CalendarPage = () => {
   return (
