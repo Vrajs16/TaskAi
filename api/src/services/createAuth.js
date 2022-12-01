22e1da85f3a74ffcf1691c7eed2dc26f1a6fad49
dilipnjit <dc575@njit.edu>
2022-12-01 16:47:03 -0500

    created fetch function to get URL with the token code

---
+++ api/src/services/createAuth.js
@@ -0,0 +1,11 @@
+import fetch from 'cross-undici-fetch'
+
+export const getAuthorizationURL = async () =>
+{
+  const res = await fetch('http://localhost:8911/createAuthorization')
+  const json = await res.json()
+
+  return {
+    url: json.data,
+  }
+}
