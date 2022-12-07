de3262fd999a7df45bdb09b0477c2eb529bed768
Mohamed Rachid <mohamednrachid@gmail.com>
2022-12-07 02:34:06 -0500

    Refactored Sign In Page UI Design
    Refactored the sign in page design to look like the old sign in page.

--- web/src/pages/LoginPage/LoginPage.js
+++ web/src/pages/LoginPage/LoginPage.js
@@ -1,5 +1,22 @@
-import { useRef } from 'react'
-import { useEffect } from 'react'
+import { useRef, useEffect, useState } from 'react'
+// import { useEffect } from 'react'
+
+import {
+  Heading,
+  Input,
+  Button,
+  InputGroup,
+  Stack,
+  InputLeftElement,
+  chakra,
+  Box,
+  Avatar,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  InputRightElement,
+} from '@chakra-ui/react'
+import { FaLock, FaUserAlt } from 'react-icons/fa'
 
 import { useAuth } from '@redwoodjs/auth'
 import {
@@ -14,8 +31,15 @@ import { Link, navigate, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 import { toast, Toaster } from '@redwoodjs/web/toast'
 
+const CFaUserAlt = chakra(FaUserAlt)
+const CFaLock = chakra(FaLock)
+
 const LoginPage = () => {
   const { isAuthenticated, logIn } = useAuth()
+
+  // Handle showing the password upon icon click
+  const [showPassword, setShowPassword] = useState(false)
+  const handleShowClick = () => setShowPassword(!showPassword)
 
   useEffect(() => {
     if (isAuthenticated) {
@@ -46,82 +70,88 @@ const LoginPage = () => {
 
       <main className="rw-main">
         <Toaster toastOptions={{ className: 'rw-toast', duration: 6000 }} />
-        <div className="rw-scaffold rw-login-container">
-          <div className="rw-segment">
-            <header className="rw-segment-header">
-              <h2 className="rw-heading rw-heading-secondary">Login</h2>
-            </header>
-
-            <div className="rw-segment-main">
-              <div className="rw-form-wrapper">
-                <Form onSubmit={onSubmit} className="rw-form-wrapper">
-                  <Label
-                    name="username"
-                    className="rw-label"
-                    errorClassName="rw-label rw-label-error"
+        <Form>
+          <Stack
+            flexDir="column"
+            mb="2"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Avatar bg="blue.500" />
+            <Heading color="blue.500">TaskAi</Heading>
+            <Box minW={{ base: '90%', md: '468px' }}>
+              <form>
+                <Stack
+                  spacing={4}
+                  p="1rem"
+                  backgroundColor="whiteAlpha.900"
+                  boxShadow="md"
+                >
+                  <FormControl isRequired>
+                    <FormLabel>Email</FormLabel>
+                    <InputGroup>
+                      <InputLeftElement
+                        pointerEvents="none"
+                        children={<CFaUserAlt color="gray.300" />}
+                      />
+                      <Input type="email" placeholder="username" />
+                    </InputGroup>
+                  </FormControl>
+                  <FormControl isRequired>
+                    <FormLabel>Password</FormLabel>
+                    <InputGroup>
+                      <InputLeftElement
+                        pointerEvents="none"
+                        color="gray.300"
+                        children={<CFaLock color="gray.300" />}
+                      />
+                      <Input
+                        type={showPassword ? 'text' : 'password'}
+                        placeholder="Password"
+                      />
+                      <InputRightElement width="4.5rem">
+                        <Button h="1.75rem" size="sm" onClick={handleShowClick}>
+                          {showPassword ? 'Hide' : 'Show'}
+                        </Button>
+                      </InputRightElement>
+                    </InputGroup>
+                    <FormHelperText textAlign="right">
+                      <Link color="blue.500" to={routes.forgotPassword()}>
+                        Forgot Password?
+                      </Link>
+                    </FormHelperText>
+                  </FormControl>
+                  <Button
+                    borderRadius={0}
+                    type="submit"
+                    variant="solid"
+                    colorScheme="blue"
+                    width="full"
                   >
-                    Username
-                  </Label>
-                  <TextField
-                    name="username"
-                    className="rw-input"
-                    errorClassName="rw-input rw-input-error"
-                    ref={usernameRef}
-                    validation={{
-                      required: {
-                        value: true,
-                        message: 'Username is required',
-                      },
-                    }}
-                  />
-
-                  <FieldError name="username" className="rw-field-error" />
-
-                  <Label
-                    name="password"
-                    className="rw-label"
-                    errorClassName="rw-label rw-label-error"
-                  >
-                    Password
-                  </Label>
-                  <PasswordField
-                    name="password"
-                    className="rw-input"
-                    errorClassName="rw-input rw-input-error"
-                    autoComplete="current-password"
-                    validation={{
-                      required: {
-                        value: true,
-                        message: 'Password is required',
-                      },
-                    }}
-                  />
-
-                  <div className="rw-forgot-link">
-                    <Link
-                      to={routes.forgotPassword()}
-                      className="rw-forgot-link"
+                    Sign in
+                  </Button>
+                  {/* <Link to={routes.signup()} className="rw-link">
+                    <Button
+                      borderRadius={0}
+                      type="button"
+                      variant="solid"
+                      colorScheme="blue"
+                      width="full"
                     >
-                      Forgot Password?
-                    </Link>
-                  </div>
-
-                  <FieldError name="password" className="rw-field-error" />
-
-                  <div className="rw-button-group">
-                    <Submit className="rw-button rw-button-blue">Login</Submit>
-                  </div>
-                </Form>
-              </div>
-            </div>
-          </div>
-          <div className="rw-login-link">
-            <span>Don&apos;t have an account?</span>{' '}
-            <Link to={routes.signup()} className="rw-link">
-              Sign up!
-            </Link>
-          </div>
-        </div>
+                      Don&apos;t have an account? Sign up!
+                    </Button>
+                  </Link> */}
+                  <p id="sign-up-link-wrapper">
+                    Don&apos;t have an account?&nbsp;
+                    <a href={routes.signup()} className="sign-up-link">
+                      Sign up!
+                    </a>
+                  </p>
+                </Stack>
+              </form>
+            </Box>
+          </Stack>
+        </Form>
       </main>
     </>
   )
