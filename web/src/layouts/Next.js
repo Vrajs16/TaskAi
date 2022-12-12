22360aaf6577b3d6a430d030ff393860076a8bc3
Vraj Shah <vrajs16108@gmail.com>
2022-12-12 18:07:05 -0500

    Fixed some merge conflicts for micheals changes

--- web/src/layouts/Next.js
+++ web/src/layouts/Next.js
@@ -5,7 +5,7 @@ import theme from './theme'
 export default class Document extends NextDocument {
   render() {
     return (
-      <Html lang='en'>
+      <Html lang="en">
         <Head />
         <body>
           {/* 👇 Here's the script */}
