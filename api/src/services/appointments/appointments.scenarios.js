948a515fa9f56bdb8d5a5ff1b9fbd407a95fd0c3
dilipnjit <dc575@njit.edu>
2022-12-11 23:29:54 -0500

    destoyed appointments

--- api/src/services/appointments/appointments.scenarios.js
+++
@@ -1,6 +0,0 @@
-export const standard = defineScenario({
-  appointment: {
-    one: { data: { userID: 4056276, title: 'String', description: 'String' } },
-    two: { data: { userID: 7278696, title: 'String', description: 'String' } },
-  },
-})
