2096de99e541c8775958f5bedc8b6fd030e45923
Michael Daley <mdaley08@outlook.com>
2022-12-11 20:03:19 -0500

    merged with dev

--- api/src/functions/auth.js
+++ api/src/functions/auth.js
@@ -2,7 +2,6 @@ import { DbAuthHandler } from '@redwoodjs/api'
 
 import { db } from 'src/lib/db'
 
-const nodemailer = require('nodemailer')
 export const handler = async (event, context) => {
   const forgotPasswordOptions = {
     // handler() is invoked after verifying that a user was found with the given
@@ -17,43 +16,20 @@ export const handler = async (event, context) => {
     // You could use this return value to, for example, show the email
     // address in a toast message so the user will know it worked and where
     // to look for the email.
-    handler: async (user) => {
-      try {
-        let transporter = nodemailer.createTransport({
-          host: 'smtp-relay.sendinblue.com',
-          port: 587,
-          secure: false,
-          auth: {
-            user: 'rachidmohamed0531@gmail.com',
-            pass: process.env.SEND_IN_BLUE_KEY,
-          },
-        })
-        const resetLink = `localhost:8910/reset-password?resetToken=${user.resetToken}`
-        const message = {
-          from: 'noreply@taskai.com',
-          to: user.email,
-          subject: 'Reset Forgotten Password',
-          text: `<a href="${resetLink}">Here is a link reset your password.  It will expire in 4hrs. <a href="${resetLink}">Reset my Password</>`,
-          html: `<a href="${resetLink}">Here is a link reset your password.  It will expire in 4hrs. <a href="${resetLink}">Reset my Password</>`,
-        }
-        await transporter.sendMail(message)
-      } catch (err) {
-        console.log(err)
-      }
-
+    handler: (user) => {
       return user
     },
 
     // How long the resetToken is valid for, in seconds (default is 24 hours)
-    expires: 60 * 60 * 4,
+    expires: 60 * 60 * 24,
 
     errors: {
       // for security reasons you may want to be vague here rather than expose
       // the fact that the email address wasn't found (prevents fishing for
       // valid email addresses)
-      usernameNotFound: 'Email not found',
+      usernameNotFound: 'Username not found',
       // if the user somehow gets around client validation
-      usernameRequired: 'Email is required',
+      usernameRequired: 'Username is required',
     },
   }
 
@@ -92,7 +68,7 @@ export const handler = async (event, context) => {
     // in. Return `false` otherwise, and in the Reset Password page redirect the
     // user to the login page.
     handler: (_user) => {
-      // return true
+      return true
     },
 
     // If `false` then the new password MUST be different from the current one
@@ -132,7 +108,7 @@ export const handler = async (event, context) => {
           email: username,
           hashedPassword: hashedPassword,
           salt: salt,
-          name: userAttributes.name,
+          // name: userAttributes.name
         },
       })
     },
--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,9 @@
+import { AuthProvider } from '@redwoodjs/auth'
+
 import { Auth0Provider } from '@auth0/auth0-react'
 import { useAuth0 } from '@auth0/auth0-react'
 import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react'
 
-import { AuthProvider } from '@redwoodjs/auth'
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
