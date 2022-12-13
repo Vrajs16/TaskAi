cac0133d60aacf710fd240490c705585a44f16b6
Eden Dubrovsky <ed239@njit.edu>
2022-12-12 22:12:22 -0500

    HomePage Content and Tests
    Removed Planner button on HomePage and added images with unit tests

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -103,35 +103,28 @@ const HomePage = () => {
                 </Link>
               </div>
             )}
-
-            {isAuthenticated ? (
-              <div>
-                {currentUser.isVerified ? (
-                  <div>
-                    <Link to={routes.planner()}>
-                      <Button rounded={'full'} px={6}>
-                        Planner
-                      </Button>
-                    </Link>
-                  </div>
-                ) : (
-                  <div>
-                    <Button onClick={() => verifying()} rounded={'full'} px={6}>
-                      Planner
-                    </Button>
-                  </div>
-                )}
-              </div>
-            ) : (
-              <div>
-                <Link to={routes.login()}>
-                  <Button rounded={'full'} px={6}>
-                    Planner
-                  </Button>
-                </Link>
-              </div>
-            )}
           </Stack>
+          <Stack direction='row'>
+           <Image
+             role='PlanningImage'
+             maxW='500px'
+             objectFit='cover'
+             src='https://www.aihr.com/wp-content/uploads/succession-planning-.png'
+             alt='Planning'
+            />
+           <Image
+             role='CompletingImage'
+             maxW='500px'
+             padding='50px'
+             objectFit='cover'
+             src='https://img.freepik.com/premium-vector/completed-task-work-done-happy-woman-worker-business-secretary-cartoon-illustration_385073-97.jpg'
+             alt='Completing'
+            />
+           <Image
+             role='SuccessImage'
+             maxW='500px' src='https://media.istockphoto.com/id/1143048152/vector/vector-illustration-representation-team-as-whole.jpg?s=612x612&w=0&k=20&c=LuAHw3MnTXymKfw7Jlh7zEmxL2_5hmVnrm3O9CkC6iQ='
+             alt='Success' />
+         </Stack>
         </Stack>
       </Container>
     </>
