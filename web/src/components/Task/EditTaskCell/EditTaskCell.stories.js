2d29d58006b7beb1088f8c26751404b38436d1e3
Maizzet <77761372+Maizzet@users.noreply.github.com>
2022-12-14 00:52:36 -0500

    Add Stories to some Task components
    Some work properly, others are set to render empty on storybook

---
+++ web/src/components/Task/EditTaskCell/EditTaskCell.stories.js
@@ -0,0 +1,9 @@
+import { Empty } from '../TaskCell'
+
+import EditTaskCell from './NewTask'
+
+export const generated = () => {
+  return <Empty />
+}
+
+export default { title: 'Components/EditTaskCell' }
